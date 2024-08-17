b6d616b86099bd4d94ec5f0681c8042ddbf35b9f
guillermo.carrillo1 <guillermo.carrillo@nter.es>
2024-08-17 15:46:31 +0200

    added user registration, login and logout, templates pending

--- home/views.py
+++ home/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+def home(request):
+    return render(request, 'home/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'home/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'home/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
