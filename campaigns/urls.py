711046210ef3ad503078ef18333af7850d9637d8
guillermo.carrillo1 <guillermo.carrillo@nter.es>
2024-08-22 13:48:18 +0200

    urls fixes

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-app_name = 'campaigns'
-
 urlpatterns = [
-    path('', views.campaign_detail, name = 'campaign_detail'),
+    path('create', views.create_campaign, name = 'create_campaign'),
+    path('<str:username>/<str:campaign_name>', views.campaign_detail, name = 'campaign_detail'),
+    path('<str:username>/<str:campaign_id>/characters', include('characters.urls')),
+    path('<str:username>/<str:campaign_id>/scenes', include('scenes.urls')),
 ]
--- campaigns/views.py
+++ campaigns/views.py
@@ -3,7 +3,6 @@ from .forms import CampaignForm
 from .models import Campaign
 from scenes.models import Scene
 from home.models import UserProfile
-from utils import get_db_handle, MongoClient
 
 def campaign_detail(request, username, campaign_name):
     user = get_object_or_404(UserProfile, username = username)
@@ -12,20 +11,11 @@ def campaign_detail(request, username, campaign_name):
     return render(request, 'campaigns/campaign_detail.html', {'campaign': campaign, 'scenes': scenes})
 
 def create_campaign(request):
-    db_handle, client = get_db_handle(
-        # aqui va la URI de mongo
-        db_name = "d6player",
-        host = "localhost",
-        port = 27017,
-        )
-    collection = db_handle['campaigns']
-    
     if request.method == 'POST':
         form = CampaignForm(request.POST)
         if form.is_valid():
             campaign_data = form.cleaned_data
             campaign_data['user'] = request.user.username
-            collection.insert_one(campaign_data)
             return redirect('home:home')
     else:
         form = CampaignForm()
